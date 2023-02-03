3780dd253cf0e1c6cac4571141dcb2932f2f8454
vinicius <vinicius.quadros@faciplac.edu>
2023-02-02 21:51:44 -0300

    Melhorando a aparencia

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const submitBtn = document.getElementById("submit");
-    // Obtém o formulário de login
-    const formLogin = document.querySelector("#form-login");
+// Obtém o formulário de login
+const formLogin = document.querySelector("#form-login");
 
-    // Adiciona um evento de submit ao formulário de login
-    formLogin.addEventListener("submit", function(event) {
-      event.preventDefault();
+formLogin.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-      // Obtém os dados do formulário
-      const usuario = document.querySelector("#usuario").value;
-      const senha = document.querySelector("#senha").value;
+  // Obtém os dados do formulário
+  const usuario = document.querySelector("#usuario").value;
+  const senha = document.querySelector("#senha").value;
 
-      // Valida os dados do formulário
-      if (usuario === "admin" && senha === "admin") {
-        // Redireciona para a página de agendamento
-        window.location.href = "app.html";
-      } else {
-        alert("Usuário ou senha inválidos");
-      }
-    });
+  // Valida os dados do formulário
+  if (usuario === "admin" && senha === "admin") {
+    window.location.href = "app.html";
+  } else {
+    alert("Usuário ou senha inválidos");
+  }
+});
